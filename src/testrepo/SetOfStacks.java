fa3395b9dd492f71569022d93052d8f8f92babb1
Roshan Rajan <roshan.ferrari@gmail.com>
2015-11-02 22:25:50 -0800

    Initial Program for multistack

---
+++ src/testrepo/SetOfStacks.java
@@ -0,0 +1,92 @@
+package testrepo;
+
+import java.util.ArrayList;
+import java.io.*;
+public class SetOfStacks {
+	
+	ArrayList<StacksImpl> stacks=new ArrayList<StacksImpl>();
+	public int capacity;
+	public SetOfStacks(int capacity){
+		this.capacity=capacity;
+	}
+	
+	public StacksImpl getLast(){
+		if (stacks.size()==0)return null;
+		return stacks.get(stacks.size()-1);
+	}
+	
+	public void push(int v){
+		StacksImpl pussh = getLast();
+		
+		if (pussh!=null && pussh.size<capacity)pussh.push(v);
+		else{
+			StacksImpl newstack= new StacksImpl(capacity);
+			newstack.push(v);
+			stacks.add(newstack);
+		}
+	}
+	
+	public int pop(){
+		StacksImpl last=getLast();
+		int v= last.pop();
+		if (last.size == 0)stacks.remove(stacks.size()-1);
+		return v;
+	}
+	
+	public int popAt(int index){
+		return leftShift(index, true);
+	}
+	
+	public int leftShift(int index,boolean isTop){
+		StacksImpl stack=stacks.get(index);
+		int removedItem;
+		if(isTop)removedItem=stack.pop();
+		else removedItem=stack.removeBottom();
+		if (stack.isEmpty()){
+			stacks.remove(index);
+		}else if(stacks.size()>index+1){
+			int value=leftShift(index+1,false);
+			stack.push(value);
+		}
+		return removedItem;	
+	}
+	
+	public static void main(String args[])throws IOException{
+		BufferedReader buff= new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the capacity of the stacks");
+		int capa=Integer.parseInt(buff.readLine());
+		SetOfStacks test= new SetOfStacks(capa);
+		int cho;
+		do{
+		System.out.println("1) Push");
+		System.out.println("2) Pop Top Of");
+		
+		int ch = Integer.parseInt(buff.readLine());
+		switch (ch) {
+		case 1:
+			System.out.println("Enter the element to be pushed");
+			int push=Integer.parseInt(buff.readLine());
+			test.push(push);
+			break;
+		case 2:
+			System.out.println("Enter the Index to be popped");
+			int popp=Integer.parseInt(buff.readLine());
+			test.popAt(popp);
+			break;
+		}
+		
+		for (int i=0;i<test.stacks.size();i++){
+			StacksImpl ack=test.stacks.get(i);
+			for(int j=0;j<ack.size;j++){
+				ack.Display();
+			}
+			
+		}
+		System.out.println("Continue");
+		 cho = Integer.parseInt(buff.readLine());
+		}while(cho==1);	
+	}
+		
+}
+
+
---
+++ src/testrepo/StacksImpl.java
@@ -0,0 +1,87 @@
+package testrepo;
+
+
+
+public class StacksImpl {
+	
+	private int capacity;
+	public Node top,bottom;
+	public int size=0;
+	public StacksImpl(int capacity){
+		this.capacity=capacity;
+	}
+	
+	public boolean isFull(){
+		return capacity==size;
+	}
+	
+	public void join (Node above,Node below){
+		if (above!=null)above.below=below;
+		if (below!=null)below.above=above;
+	}
+	
+	public boolean push(int v){
+		if (size>=capacity)return false;
+		size++;
+		Node n=new Node(v);
+		if (size == 1) bottom=n;
+		join(n,top);
+		top=n;
+		return true;
+	}
+	
+	public int pop(){
+		Node t= top;
+		top= top.below;
+		size--;
+		return t.data;
+	}
+	
+	public boolean isEmpty(){
+		return size==0;
+	}
+	
+	public int removeBottom(){
+		Node b= bottom;
+		bottom=bottom.above;
+		if (bottom!=null) bottom.below=null;
+		size --;
+		return b.data;
+	}
+	
+	public void Display(){
+		Node current=top;
+			do{
+				System.out.println(current.data);
+				if (current.below!=null)
+			current=current.below;}
+			while(current.below!=null);
+		
+	}
+	
+	private class Node{
+		Node above,below;
+		int data;
+		public Node(int _data){
+			above=null;
+			below=null;
+			data=_data;
+		}
+//		public Node(Node setNext,String _data){
+//			above=setNext;
+//			data=_data;
+//		}
+//		public int getData(){
+//			return data;
+//		}
+//		public void setData(int _data){
+//			data=_data;
+//		}
+//		public void setNext(Node setNext){
+//			above=setNext;
+//		}
+//		public Node getNext(){
+//			return above;
+//		}
+	}
+}
